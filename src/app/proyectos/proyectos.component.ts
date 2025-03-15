7f40e524af2f78563982548c16d43cd8805feec6
oscarin <oscar45medina@gmail.com>
2025-03-15 02:27:42 -0300

    actualizacion de pagina

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -9,15 +9,16 @@ import { Component } from '@angular/core';
 })
 export class ProyectosComponent {
   proyectos = [
-    { nombre: 'Ejecución 1', url: 'https://oscarin12.github.io/EJECICIOS1/', imagen: 'assets/img/image.png' },
-    { nombre: 'Ejecución 2', url: 'https://oscarin12.github.io/EJECICIOS2/', imagen: 'assets/img/imagen2.png' },
-    { nombre: 'Ejecución 4', url: 'https://oscarin12.github.io/Ejercicio4/', imagen: 'assets/img/imagen3.png' },
-    { nombre: 'Ejecución 5', url: 'https://oscarin12.github.io/Ejercicio5/', imagen: 'assets/img/imagen4.png' },
-    { nombre: 'Ejecución 6', url: 'https://oscarin12.github.io/Ejercicio6/', imagen: 'assets/img/imagen5.png' },
+    { nombre: 'Ejecución 1', url: 'https://oscarin12.github.io/EJECICIOS1/', url1:'https://github.com/oscarin12/EJECICIOS1', imagen: 'assets/img/image.png' },
+    { nombre: 'Ejecución 2', url: 'https://oscarin12.github.io/EJECICIOS2/',url1:'https://github.com/oscarin12/ejercicio2', imagen: 'assets/img/imagen2.png' },
+    { nombre: 'Ejecución 3', url: 'https://github.com/oscarin12/ejercicio3',url1:'https://github.com/oscarin12/ejercicio3', imagen: 'assets/img/ejer3.jpg' },
+    { nombre: 'Ejecución 4', url: 'https://oscarin12.github.io/Ejercicio4/',url1:'https://github.com/oscarin12/ejercicio4', imagen: 'assets/img/imagen3.png' },
+    { nombre: 'Ejecución 5', url: 'https://oscarin12.github.io/Ejercicio5/',url1:'https://github.com/oscarin12/ejercicio5', imagen: 'assets/img/imagen4.png' },
+    { nombre: 'Ejecución 6', url: 'https://oscarin12.github.io/Ejercicio6/',url1:'https://github.com/oscarin12/ejercicio6', imagen: 'assets/img/imagen5.png' },
     { nombre: 'Ejecución 7', url: 'https://oscarin12.github.io/botones/', imagen: 'assets/img/botones.jpg' },
     { nombre: 'Próximo Proyecto', url: '#', imagen: 'assets/img/pro.jpg' },
-    { nombre: 'Próximo Proyecto', url: '#', imagen: 'assets/img/pro.jpg' },
-    { nombre: 'Próximo Proyecto', url: '#', imagen: 'assets/img/pro.jpg' },
+    { nombre: 'Próximo Proyecto', url: '#', imagen: 'assets/img/pro.jpg' }
+    
    
     
   ];
