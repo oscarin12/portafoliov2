db4e7b35b37d66a649d10df21039bd371363a4e8
oscarin <oscar45medina@gmail.com>
2025-03-22 15:38:21 -0300

    actualizacion de proyectos

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class ProyectosComponent {
   proyectos = [
+    { nombre: 'Próximo Proyecto', url: 'https://oscarin12.github.io/rick-and-morty-test/',url1:'https://github.com/oscarin12/rick-and-morty-test', imagen: 'assets/img/rick.jpg' },
     { nombre: 'Ejecución 1', url: 'https://oscarin12.github.io/EJECICIOS1/', url1:'https://github.com/oscarin12/EJECICIOS1', imagen: 'assets/img/image.png' },
     { nombre: 'Ejecución 2', url: 'https://oscarin12.github.io/EJECICIOS2/',url1:'https://github.com/oscarin12/ejercicio2', imagen: 'assets/img/imagen2.png' },
     { nombre: 'Ejecución 3', url: 'https://github.com/oscarin12/ejercicio3',url1:'https://github.com/oscarin12/ejercicio3', imagen: 'assets/img/ejer3.jpg' },
@@ -16,8 +17,7 @@ export class ProyectosComponent {
     { nombre: 'Ejecución 5', url: 'https://oscarin12.github.io/Ejercicio5/',url1:'https://github.com/oscarin12/ejercicio5', imagen: 'assets/img/imagen4.png' },
     { nombre: 'Ejecución 6', url: 'https://oscarin12.github.io/Ejercicio6/',url1:'https://github.com/oscarin12/ejercicio6', imagen: 'assets/img/imagen5.png' },
     { nombre: 'Ejecución 7', url: 'https://oscarin12.github.io/botones/', imagen: 'assets/img/botones.jpg' },
-    { nombre: 'Próximo Proyecto', url: '#', imagen: 'assets/img/pro.jpg' },
-    { nombre: 'Próximo Proyecto', url: '#', imagen: 'assets/img/pro.jpg' }
+        { nombre: 'Próximo Proyecto', url: '#', imagen: 'assets/img/pro.jpg' }
     
    
     
